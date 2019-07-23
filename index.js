8ccbd9a50854cd94e61ced5fc35ec3bfef987587
eeshwarankathi <eeshwarankathi@gmail.com>
2019-07-23 20:56:05 +0530

    2 files modified

--- index.js
+++ index.js
@@ -22,22 +22,61 @@ xhttp.onreadystatechange = function() {
     var averageSpeed = 0;
     var elevationGained = 0;
     var movingTime = 0;
+    var idleTime = 0;
     var totalTime = 0;
-    var prevLat, prevLong, prevAlt, prevDate;
+    var prevLat, prevLong, prevAlt, prevTime;
     for(let i in elements) {
       let element = elements[i];
-      let lat = parseFloat(element.attributes['lat'].value);
-      let lon = parseFloat(element.attributes['lon'].value);
-      let alt = parseFloat(element.children[0].innerHTML);
-      let time = new Date(element.children[1].innerHTML).getTime();
-      if(prevLat && prevLong && prevAlt && prevDate) {
-        
+      let attributes = element.attributes;
+      let children = element.children;
+      var lat, lon, alt, time;
+      if(attributes && children) {
+        let lat = parseFloat(attributes['lat'].value);
+        let lon = parseFloat(attributes['lon'].value);
+        let alt = parseFloat(children[0].innerHTML);
+        let time = new Date(children[1].innerHTML).getTime();
+        if(prevLat && prevLong && prevAlt && prevTime) {
+          elevationGained = elevationGained + alt - prevAlt;
+          let ctime = (time-prevTime)/1000;
+          if(prevLat == lat && prevLong == lon && prevAlt == alt) {
+            idleTime += ctime;
+          }else {
+            let dist = findDistance(prevLat, prevLong, prevAlt, lat, lon, alt);
+            totalDistance += dist;
+            let speed = dist/ctime;
+            if(speed > maxSpeed) {
+              maxSpeed = speed;
+            }
+          }
+          totalTime += ctime;
+        }
+        prevLat = lat;
+        prevLong = lon;
+        prevAlt = alt;
+        prevTime = time;
       }
     }
+    averageSpeed = (totalDistance/totalTime);
+    movingTime = totalTime - idleTime;
+    var solution1 = 'Total Distance: '+totalDistance;
+    solution1 += 'm <br>Max Speed: '+maxSpeed;
+    solution1 += 'm/s <br>Average Speed: '+averageSpeed;
+    solution1 += 'm/s <br>Elevation Gained: '+elevationGained;
+    solution1 += 'm <br>Moving Time: '+movingTime;
+    solution1 += 's <br>Total time Elapsed: '+totalTime;
+    solution1 += 's';
+    appDiv.innerHTML = solution1;
   }
 };
 xhttp.open("GET", "https://dl.dropboxusercontent.com/s/8nvqnasci6l76nz/Problem.gpx", true);
 xhttp.send();
 
-
-var txt = '<trkpt lat="13.1935950" lon="77.6491150"><ele>922.5</ele><time>2016-12-11T00:37:52Z</time></trkpt>';
+function findDistance(plat, plon, palt, lat, lon, alt) {
+  let px = palt * Math.cos(plat) * Math.sin(plon);
+  let py = palt * Math.sin(plat);
+  let pz = palt * Math.cos(plat) * Math.cos(plon);
+  let x = alt * Math.cos(lat) * Math.sin(lon);
+  let y = alt * Math.sin(lat);
+  let z = alt * Math.cos(lat) * Math.cos(lon);
+  return Math.sqrt(((px-x)*(px-x)) + ((py-y)*(py-y)) + ((pz-z)*(pz-z)));
+}
